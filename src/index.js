085c90e95d00834e6f656ea763a753bdb789cf07
IhorHudkov <55943514+IhorHudkov@users.noreply.github.com>
2021-09-15 14:47:46 +0300

    Add socket.io events with broadcasting

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 const express = require("express");
-
-const app = express();
+const socketio = require("socket.io");
+const http = require("http");
 
 const port = process.env.PORT || 3000;
 
-app.use(express.static('./public'));
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
-app.listen(port, () => {
+app.use(express.static("./public"));
+
+io.on("connection", (socket) => {
+  socket.emit("message", "Welcome!");
+  socket.broadcast.emit("message", "A new user has joined!");
+
+  socket.on("sendMessage", (message) => {
+    io.emit("message", message);
+  });
+
+  socket.on("disconnect", () => {
+    io.emit("message", "A user has left!");
+  });
+});
+
+server.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
