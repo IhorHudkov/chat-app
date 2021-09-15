bcdc8ed312bb960df919904d99fb780aa331e6ae
IhorHudkov <55943514+IhorHudkov@users.noreply.github.com>
2021-09-15 17:02:42 +0300

    Add sharing your location

--- public/js/chat.js
+++ public/js/chat.js
@@ -9,3 +9,17 @@ document.querySelector("#message-form").addEventListener("submit", (e) => {
 socket.on("message", (message) => {
   console.log(message);
 });
+
+document.querySelector("#send-location").addEventListener("click", () => {
+  if (!navigator.geolocation) {
+    return alert("Geolocation is not supported by your browser");
+  }
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    const coords = {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude,
+    };
+    socket.emit("sendLocation", coords);
+  });
+});
